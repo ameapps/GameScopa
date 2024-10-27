df80e1df2d9e1b46869ff977d6bf4facdacdca85
Stefano <git.stefano@outlook.it>
2024-10-27 19:33:41 +0100

    App: aggiunto componenti condivisi

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
-
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SharedRoutingModule
-  ]
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, SharedRoutingModule],
+  exports: [HeaderComponent, FooterComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
