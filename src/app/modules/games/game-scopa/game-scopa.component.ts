a8e5d46337680f9626263d6f42e7d06cd49b978c
Ste <git.stefano@outlook.it>
2025-01-02 08:57:18 +0100

    Game: è possibile fare drag and drop di una carta del giocatore sul tavolo

--- src/app/modules/games/game-scopa/game-scopa.component.ts
+++ src/app/modules/games/game-scopa/game-scopa.component.ts
@@ -8,8 +8,7 @@ import { GameService } from 'src/app/shared/services/game/game.service';
   styleUrls: ['./game-scopa.component.scss'],
 })
 export class GameScopaComponent implements OnInit {
-
-  constructor(public game_service: GameService, public common: CommonService) { }
+  constructor(public game_service: GameService, public common: CommonService) {}
 
   ngOnInit() {
     //1. Inizializzo le carte del gioco secondo le impostazioni definite
@@ -18,12 +17,42 @@ export class GameScopaComponent implements OnInit {
 
   private getGameCards() {
     this.game_service.startGame(this.common.currentActiveGame);
-    this.game_service.tableCards = this.game_service.getTableCards(this.common.currentActiveGame);
-    this.game_service.playerCards = this.game_service.getPlayerCards(this.common.currentActiveGame);
+    this.game_service.tableCards = this.game_service.getTableCards(
+      this.common.currentActiveGame
+    );
+    this.game_service.playerCards = this.game_service.getPlayerCards(
+      this.common.currentActiveGame
+    );
   }
 
   shuffleCards() {
     this.getGameCards();
   }
 
+  // #region drag and drop
+  onDragStart(event: DragEvent, cardIndex: number) {
+    // Salviamo l'indice della carta trascinata nei dati del drag event
+    event.dataTransfer?.setData('text/plain', cardIndex.toString());
+  }
+
+  allowDrop(event: DragEvent) {
+    // Previene il comportamento predefinito per abilitare il drop
+    event.preventDefault();
+  }
+
+  onDrop(event: DragEvent) {
+    // Recuperiamo l'indice della carta trascinata
+    event.preventDefault();
+    const cardIndex = event.dataTransfer?.getData('text/plain');
+    if (cardIndex !== null) {
+      // Convertiamo l'indice in numero
+      const index = parseInt(cardIndex ?? '-1', 10);
+      // Rimuoviamo la carta dall'array delle carte del giocatore
+      const card = this.game_service.playerCards.splice(index, 1)[0];
+      // Aggiungiamo la carta all'array delle carte sul tavolo
+      this.game_service.tableCards.push(card);
+    }
+  }
+
+  //#endregion
 }
